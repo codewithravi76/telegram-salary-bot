1b043879d1fbfae85cbb9072c6aa68e93ed8c81f
codewithravi76 <codewithravi76@gmail.com>
2026-05-10 13:28:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import telebot
+from datetime import datetime
+import os
+
+BOT_TOKEN = os.getenv("8708661936:AAEKDfMJvflGkEDXeFr2tvU9tQIDa8RENNw")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['report'])
+def report(message):
+    try:
+        data = message.text.split()
+
+        username = data[1]
+        links = data[2]
+        salary = data[3]
+        status = data[4]
+
+        if status.lower() == "credited":
+            status_text = "Credited ✅"
+        else:
+            status_text = "Pending ⏳"
+
+        today = datetime.now().strftime("%d %B %Y")
+
+        text = f"""
+📢 WORK UPDATE
+
+👤 Employee: {username}
+🔗 Links Collected: {links}
+💰 Salary Earned: ₹{salary}
+💳 Salary Status: {status_text}
+📅 Date: {today}
+"""
+
+        bot.reply_to(message, text)
+
+    except:
+        bot.reply_to(message, "Usage:\n/report @username links salary credited/pending")
+
+print("Bot Running...")
+bot.infinity_polling()
